afd4e50c2c1299c92fff45f7d91f9f69ca74a764
div-styl <gouhmada@gmail.com>
2023-08-31 20:07:10 +0100

    updating label is not working due to continue(true)?!

--- src/models/build_clock.rs
+++ src/models/build_clock.rs
@@ -1,36 +1,47 @@
 use gtk4::prelude::*;
 use gtk4::{Application, ApplicationWindow};
-//use std::process::exit;
+use crate::models::time::timer;
+use std::sync::mpsc;
+use std::thread;
 
-pub fn clock(app: &Application ){
-    //create the Button
-    // let button1 = Button::builder()
-    //     .label("Welcome to GTK")
-    //     .margin_top(10)
-    //     .margin_bottom(10)
-    //     .margin_start(5)
-    //     .margin_end(5)
-    //     .width_request(100)
-    //     .build();
-    // //let button2
-    // // connect the Button
-    // button1.connect_clicked(|button1: &Button|{
-    //     button1.set_label("Noob");
-    //     eprintln!("Button is killed");
-    //     exit(0);
-    // });
 
-    // create ApplicationWindow
+pub fn clock(app: &Application) {
+    let clock_label = gtk4::Label::new(None);
+
+    // Create ApplicationWindow
     let win = ApplicationWindow::builder()
         .application(app)
-        .title("into")
+        .title("Clock App")
+        .child(&clock_label) // Add the label to the window
         .build();
-    // apply Css
+
+    // Create a channel to send updates from the timer thread
+    let (tx, rx) = mpsc::channel();
+
+    // Clone the label for use in the closure
+    let label_clone = clock_label.clone();
+
+    // Clone the sender for the timer thread
+    let sender_clone = tx.clone();
+
+    // Apply CSS
     let css_provider = gtk4::CssProvider::new();
-    css_provider
-        .load_from_data("window { background-color: #183D3D; }");
+    css_provider.load_from_data("window { background-color: #183D3D; }");
     let style_context = win.style_context();
     style_context.add_provider(&css_provider, gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    // Spawn a new thread to update the clock label
+    thread::spawn(move || {
+        timer(sender_clone);
+    });
+
+    // Listen for updates from the timer thread and update the label
+    glib::timeout_add_seconds_local(1, move || {
+        if let Ok(formatted_time) = rx.try_recv() {
+            label_clone.set_text(&formatted_time);
+        }
+        std::ops::ControlFlow::Continue(true)
+    });
 
     // Present window
     win.present();
--- src/models/time.rs
+++ src/models/time.rs
@@ -1,26 +1,31 @@
-use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
 use chrono::prelude::*;
+use std::thread;
+
 /**
  * timer - is function which print the local time
  * @noparamter so far
  * Return: void
  */
-pub fn timer() {
-    loop {
-        let local_time = Local::now();
-        let formatted_time = format_time_am_pm(local_time);
+pub fn timer(sender: mpsc::Sender<String>) {
+    thread::spawn(move || {
+        loop {
+            let local_time = Local::now();
+            let formatted_time = format_time_am_pm(local_time);
 
-        println!("{}", formatted_time);
+            // Send the formatted time to the main thread
+            sender.send(formatted_time).unwrap();
 
-        // Wait for a second before updating the time
-        thread::sleep(Duration::from_secs(1));
-    }
+            // Wait for a second before updating the time
+            thread::sleep(std::time::Duration::from_secs(1));
+        }
+    });
 }
+
 /**
  * format_time_am_pm - function which print the format style of the clock
  * @time: dataTime local
- * Return: the tring format of the timer
+ * Return: the string format of the timer
  */
 fn format_time_am_pm(time: DateTime<Local>) -> String {
     let am_pm = if time.hour() < 12 { "AM" } else { "PM" };
